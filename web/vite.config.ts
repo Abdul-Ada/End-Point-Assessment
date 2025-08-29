705ec833f8c09b7f57c42c8a03b11152c54c1f5d
Abdul-Ada <117444400+Abdul-Ada@users.noreply.github.com>
2025-08-29 13:02:43 +0100

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // Add this 'test' section
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 })
